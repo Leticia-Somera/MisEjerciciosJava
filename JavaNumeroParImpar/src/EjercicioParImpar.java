972d92d414bb67243c2b2e5155110d9fcae8be0a
Leticia Somera <leticia.somera@gmail.com>
2022-02-09 20:01:37 -0600

    Ejercicio de selección de números pares

---
+++ JavaNumeroParImpar/src/EjercicioParImpar.java
@@ -0,0 +1,11 @@
+public class EjercicioParImpar {
+	public static void main(String[] args) {
+		int numero = 8;
+		double residuo = numero % 2;
+		
+		int resultado = (residuo == 0)? 0 : 1;
+		
+		System.out.println(resultado);
+	}
+
+}
