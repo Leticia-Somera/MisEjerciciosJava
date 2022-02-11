acdf4f04483587a657b644a9f171fb005ac7e5f3
Leticia Somera <leticia.somera@gmail.com>
2022-02-10 18:43:04 -0600

    Clases abstractas e interfaces

---
+++ JavaHerenciaYPolimorfismo/src/com/generation/animales/Gato.java
@@ -0,0 +1,20 @@
+package com.generation.animales;
+
+public class Gato extends Animal implements Mascota {
+
+	@Override
+	public void jugar() {
+		System.out.println("Estoy jugando con cajas");
+	}
+
+	@Override
+	public void correr() {
+		System.out.println("Estoy persguiendo una luz");	
+	}
+
+	@Override
+	public void baniar() {
+		System.out.println("No me gusta el agua");
+	}
+
+}
---
+++ JavaHerenciaYPolimorfismo/src/com/generation/animales/Mascota.java
@@ -0,0 +1,12 @@
+package com.generation.animales;
+
+public interface Mascota {
+	//Intrínsecamente se sabe que en una interfaz todo es público y abstracto
+	
+	void jugar();
+	
+	void correr();
+	
+	void baniar();
+
+}
